0da1a291cf8a5ba6f1aa82d7c067e4a54e502e58
Claire Marconi <claire.marconi94@gmail.com>
2022-02-22 18:11:49 +0100

    favorite toggle red working

---
+++ app/javascript/controllers/toggle_favorites_controller.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll('.like-icon');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    event.currentTarget.classList.toggle('red');
+  });
+});
