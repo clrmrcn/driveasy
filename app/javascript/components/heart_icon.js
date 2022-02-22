5e8fda13cc33c7a4b2b4ef27723dec6ad72a7769
Claire Marconi <claire.marconi94@gmail.com>
2022-02-22 19:16:18 +0100

    red toggle working on load

--- app/javascript/components/heart_icon.js
+++ app/javascript/components/heart_icon.js
@@ -1,7 +1,5 @@
-
-const buttons = document.querySelectorAll('.like-icon');
-
 const toggleRed = () => {
+  const buttons = document.querySelectorAll('.like-icon');
   buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
       event.currentTarget.classList.toggle('red');
