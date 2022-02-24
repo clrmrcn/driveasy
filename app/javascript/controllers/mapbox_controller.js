d3b49cb313b45481d1cc9425a5fb3dd305660ce2
Claire Marconi <claire.marconi94@gmail.com>
2022-02-24 10:38:20 +0100

    info window and search bar on map

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -11,7 +11,6 @@ export default class extends Controller {
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
